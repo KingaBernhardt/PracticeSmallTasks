4c59498e7f9c99bbe6352c826a32670ed7740596
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-10-15 14:43:55 +0100

    feat(Apicall):add initializeClient method

---
+++ ApiCall/ApiCall/ApiHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCall
+{
+    public static class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; };
+
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+
+            //Usually we would use a base address and append the called route to it, but now the baseaddress comes from to address so I leave it blank.
+            //ApiClient.BaseAddress = new Uri("http://xkcd.com/");
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
