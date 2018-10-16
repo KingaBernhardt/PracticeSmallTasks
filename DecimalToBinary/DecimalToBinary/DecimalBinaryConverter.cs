9b1e11a8561f817a496b2b406b9dfe493bd44855
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-10-16 12:47:12 +0100

    feat(decimalToBinary)add method and testcase

---
+++ DecimalToBinary/DecimalToBinary/DecimalBinaryConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DecimalToBinary
+{
+    class DecimalBinaryConverter
+    {
+        public string ConvertToBinary(long decimalNumber)
+        {
+            if (decimalNumber < 0)
+            {
+                throw new ArgumentException("Only positive decimal number expected!");
+            }
+
+            long quotient;
+            long remainder;
+
+            StringBuilder binaryBuilder = new StringBuilder();
+            while (decimalNumber > 0)
+            {
+                quotient = decimalNumber / 2;
+                remainder = decimalNumber % 2;
+                decimalNumber = quotient;
+
+                binaryBuilder.Append(remainder);
+            }
+            return string.Join("", binaryBuilder.ToString().ToCharArray().Reverse());
+        }
+    }
+}
---
+++ DecimalToBinary/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private DecimalBinaryConverter binaryConverter;
+        public UnitTest1(DecimalBinaryConverter binaryConverter)
+        {
+            this.binaryConverter = binaryConverter;
+        }
+        [TestMethod]
+        public void TestConvertToBinary()
+        {
+            Assert.AreEqual("100", binaryConverter.ConvertToBinary(4));
+        }
+    }
+}
