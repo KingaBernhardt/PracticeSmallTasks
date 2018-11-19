c00394e315feb5d62b63be18e9531897e8c828c7
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-11-19 14:16:51 +0000

    feat(ArrangeWithoutUsingSort):add new list

---
+++ ArrangeWithoutUsingSort/ArrangeWithoutUsingSort/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrangeWithoutUsingSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> li = new List<int>();
+            li.Add(4);
+            li.Add(1);
+            li.Add(3);
+            li.Add(2);
+
+            foreach(var item in li)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
