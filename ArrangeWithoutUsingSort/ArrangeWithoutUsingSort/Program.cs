6ac2334e28ea094b3c25a2787bb2d531e60a03a8
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-11-19 14:28:54 +0000

    feat(ArrangeWithoutUsingSort):add function

--- ArrangeWithoutUsingSort/ArrangeWithoutUsingSort/Program.cs
+++ ArrangeWithoutUsingSort/ArrangeWithoutUsingSort/Program.cs
@@ -17,7 +17,32 @@ namespace ArrangeWithoutUsingSort
             {
                 Console.WriteLine(item);
             }
+            
+            Console.WriteLine("After sorting: ");
+         
+            foreach (var item in SortFunction(li))
+            {
+                Console.WriteLine(item);
+            }
             Console.ReadLine();
+        }
+
+        public static List<int> SortFunction(List<int> li)
+        {
+            int temp;
+            for (int i = 0; i < li.Count; i++)
+            {
+                for (int j = 0; j < li.Count; j++)
+                {
+                    if (li[i] < li[j])
+                    {
+                        temp = li[i];
+                        li[i] = li[j];
+                        li[j] = temp;
+                    }
+                }
+            }
+            return li;
         }
     }
 }
